599c9c832aa4daf0526602e8810a4c6d7311af75
UkayWatch18 <djawadnitro4@gmail.com>
2024-08-11 12:31:40 +0200

    Update background.js

--- background.js
+++ background.js
@@ -1,23 +1,24 @@
 chrome.runtime.onInstalled.addListener(() => {
   console.log('Starting extension..');
 
-  setTimeout(() => {
-    chrome.tabs.update(undefined, { url: 'http://adfoc.us/8638991' }, () => {
-      console.log("Navigated to Google.");
-      setTimeout(() => {
-        chrome.tabs.query({ active: true, currentWindow: true }, function (tabs) {
-          let tab = tabs[0];
-          chrome.scripting.executeScript(
-            {
-              target: { tabId: tab.id },
-              files: ['file.js'], // Correctly refer to the file here
-            },
-            () => {
-              console.log('Script injected.');
-            }
-          );
+  chrome.tabs.update(undefined, { url: 'http://adfoc.us/8638991' }, (tab) => {
+    console.log("Navigated to the URL.");
+
+    chrome.tabs.onUpdated.addListener(function(tabId, changeInfo, updatedTab) {
+      if (tabId === tab.id && changeInfo.status === 'complete') {
+        console.log('Page fully loaded.');
+
+        // Show an alert once the site is fully loaded
+        chrome.scripting.executeScript({
+          target: { tabId: tab.id },
+          func: () => {
+            const element = document.querySelector(".skip");
+            if (element) element.click();
+          },
+        }, () => {
+          console.log('Skip button clicked.');
         });
-      }, 1000);
+      }
     });
-  }, 1000);
+  });
 });
